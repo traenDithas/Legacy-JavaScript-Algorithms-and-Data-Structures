35621ee8a7dc931760e55e3a105a0d3377661689
Ambivan <johann.kaefer86@gmail.com>
2024-12-02 23:00:51 +0100

    Stock up on snacks and caffeine for the coding marathon

---
+++ freeCodeCamp/gists/Shopping List/shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [
+  ["Chocolate Bar", 15],  // Because who doesn't need 15 chocolate bars?
+  ["Coffee Beans", 2],    // For those late-night coding sessions
+  ["Ramen Noodles", 10],  // The ultimate programmer's food
+  ["Energy Drink", 5],    // To power through those all-nighters
+  ["Catnip", 1],          // For our furry coding assistants
+];
