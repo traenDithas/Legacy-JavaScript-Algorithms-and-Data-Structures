39fcc42ac9d9044dfc7bdf3397bbffc5429ef3f8
Ambivan <johann.kaefer86@gmail.com>
2024-12-20 15:22:56 +0100

    Add buyHealth function to purchase health with gold

--- freeCodeCamp/Slay the JavaScript Dragon/script.js
+++ freeCodeCamp/Slay the JavaScript Dragon/script.js
@@ -29,6 +29,13 @@ const locations = [
     "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
     "button functions": [buyHealth, buyWeapon, goTown],
     text: "You enter the store."
+  },
+
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
   }
 ];
 
@@ -52,11 +59,11 @@ function goTown() {
 }
 
 function goStore() {
- 
+  update(locations[1]);
 }
 
 function goCave() {
-  console.log("Going to cave.");
+  update(locations[2]);
 }
 
 function fightDragon() {
@@ -64,10 +71,24 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
 }
 
 function buyWeapon() {
 
 }
 
+function fightSlime() {
+
+}
+
+function fightBeast() {
+  
+}
