516d63756b825466606ca1d581af17222a5a05b8
Ambivan <johann.kaefer86@gmail.com>
2024-12-21 16:34:34 +0100

    Add buyWeapon function to purchase weapons with gold

--- freeCodeCamp/Slay the JavaScript Dragon/script.js
+++ freeCodeCamp/Slay the JavaScript Dragon/script.js
@@ -79,17 +79,29 @@ function fightDragon() {
 
 function buyHealth() {
   if (gold >= 10) {
-      gold -= 10;
-      health += 10;
-      goldText.innerText = gold;
-      healthText.innerText = health;
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
   } else {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
 
 function buyWeapon() {
-
+  if (currentWeaponIndex < weapons.length) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeaponIndex++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeaponIndex].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+  }
 }
 
 function fightSlime() {
