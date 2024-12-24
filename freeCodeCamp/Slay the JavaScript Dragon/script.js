2e1b13f831a4072f4fe1d5a9ab2df8e63707b528
Ambivan <johann.kaefer86@gmail.com>
2024-12-24 13:17:44 +0100

    Add monsters variable for game entities

--- freeCodeCamp/Slay the JavaScript Dragon/script.js
+++ freeCodeCamp/Slay the JavaScript Dragon/script.js
@@ -17,10 +17,16 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 const weapons = [
-  { name: 'stick', power: 5 },
-  { name: 'dagger', power: 30 },
-  { name: 'claw hammer', power: 50 },
-  { name: 'sword', power: 100 }
+  { name: "stick", power: 5 },
+  { name: "dagger", power: 30 },
+  { name: "claw hammer", power: 50 },
+  { name: "sword", power: 100 }
+];
+
+const monsters = [
+  { name: "slime", level: 2, health: 15 },
+  { name: "fanged beast", level: 8, health: 60 },
+  { name: "dragon", level: 20, health: 300 }
 ];
 
 const locations = [
