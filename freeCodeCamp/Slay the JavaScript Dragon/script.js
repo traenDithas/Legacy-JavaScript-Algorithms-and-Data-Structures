ce50ece6e636502bf51a47336b2773125c2f4083
Ambivan <johann.kaefer86@gmail.com>
2024-12-27 11:19:36 +0100

    Create defeatMonster function for handling monster defeat and updating game state

--- freeCodeCamp/Slay the JavaScript Dragon/script.js
+++ freeCodeCamp/Slay the JavaScript Dragon/script.js
@@ -168,3 +168,11 @@ function attack() {
 function dodge() {
   text.innerText = "You dodge the attack from the " + monsters[fighting].name;
 }
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
